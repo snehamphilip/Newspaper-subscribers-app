dbae86e588586dae73bab40c180955e8bb3e1931
snehamphilip <164026288+snehamphilip@users.noreply.github.com>
2024-06-29 13:18:28 +0530

    delete-subscriber

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AddPage from './components/AddPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import View from './components/View';
 import Search from './components/Search';
+import Delete from './components/Delete';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/add' element={<AddPage/>}/>
       <Route path='/view' element={<View/>}/>
       <Route path='/search' element={<Search/>}/>
+      <Route path='/delete' element={<Delete/>}/>
     </Routes>
     </BrowserRouter>
   );
